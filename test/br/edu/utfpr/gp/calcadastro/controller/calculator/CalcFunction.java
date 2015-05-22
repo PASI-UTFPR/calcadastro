ff17a99833b4f55d09cd61c1f8d21674f62ac647
Rehnan Carolino <rehnancarolino@live.com>
2015-05-22 20:15:28 -0300

    Calcultator testes refactored!

--- test/br/edu/utfpr/gp/calcadastro/controller/calculator/CalcFunction.java
+++ test/br/edu/utfpr/gp/calcadastro/controller/calculator/CalcFunction.java
@@ -7,124 +7,130 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.Test;
 import org.junit.*;
 
 public class CalcFunction {
-	
+
 	private WebDriver driver;
 	private long time = 3000;
-	
+
 	@Before
-	public void setup(){
-		//Entra na home do sistema
-		newInstanceWebDriver(new FirefoxDriver(), "http://localhost:8080/calcadatro");
+	public void setup() {
+		// Entra na home do sistema
+		newInstanceWebDriver(new FirefoxDriver(),
+				"http://localhost:8080/calcadatro");
 
 	}
-	
-	//Teste Positivo para a ação soma
+
+	// Teste Positivo para a ação soma
 	@Test
-	public void testSumSuccess(){
-		
-		//Botão calculator clicado (Redireciona para a calculadora)
+	public void testSumSuccess() {
+
+		// Botão calculator clicado (Redireciona para a calculadora)
 		getDriver().findElement(By.id("calculator")).click();
-		
+
 		String n1, n2;
-		//Resupera valores
+		// Resupera valores
 		n1 = getDriver().findElement(By.id("number1")).getText();
 		n2 = getDriver().findElement(By.id("number2")).getText();
-		
-		//Soma valores
-		getNewWebElement("result").sendKeys(String.valueOf(Integer.valueOf(n1)+Integer.valueOf(n2)));
+
+		// Soma valores
+		getNewWebElement("result").sendKeys(
+				String.valueOf(Integer.valueOf(n1) + Integer.valueOf(n2)));
 		getDriver().findElement(By.id("sum")).click();
-		
-		//Span de feedback do resultado
-		assertEquals("Resultado correto!", getDriver().findElement(By.id("message")).getText());
+
+		// Span de feedback do resultado
+		assertEquals("Resultado correto!",
+				getDriver().findElement(By.id("message")).getText());
 		waitTime(time);
 	}
-	
-	   //Teste Positivo para a ação subtração
-		@Test
-		public void testMinusSuccess(){
-			
-			String n1, n2;
-			//Resupera valores
-			n1 = getDriver().findElement(By.id("number1")).getText();
-			n2 = getDriver().findElement(By.id("number2")).getText();
-			
-			//Soma valores
-			getNewWebElement("result").sendKeys(String.valueOf(Integer.valueOf(n1)-Integer.valueOf(n2)));
-			getDriver().findElement(By.id("sum")).click();
-			
-			//Span de feedback do resultado
-			assertEquals("Resultado correto!", getDriver().findElement(By.id("message")).getText());
-			waitTime(time);
-		}
-		
-		//Teste Positivo para a ação multiplicação
-		@Test
-		public void testMultiplySuccess(){
-			
-			String n1, n2;
-			//Resupera valores
-			n1 = getDriver().findElement(By.id("number1")).getText();
-			n2 = getDriver().findElement(By.id("number2")).getText();
-			
-			//Soma valores
-			getNewWebElement("result").sendKeys(String.valueOf(Integer.valueOf(n1)*Integer.valueOf(n2)));
-			getDriver().findElement(By.id("sum")).click();
-			
-			//Span de feedback do resultado
-			assertEquals("Resultado correto!", getDriver().findElement(By.id("message")).getText());
-			waitTime(time);
-		}
-		
-		//Teste Positivo para a ação divide
-		@Test
-		public void testDivideSuccess(){
-			
-			String n1, n2;
-			//Resupera valores
-			n1 = getDriver().findElement(By.id("number1")).getText();
-			n2 = getDriver().findElement(By.id("number2")).getText();
-			
-			//Soma valores
-			getNewWebElement("result").sendKeys(String.valueOf(Integer.valueOf(n1)/Integer.valueOf(n2)));
-			getDriver().findElement(By.id("sum")).click();
-			
-			//Span de feedback do resultado
-			assertEquals("Resultado correto!", getDriver().findElement(By.id("message")).getText());
-			waitTime(time);
-		}
-		
-		
 
-	
-	private WebDriver getDriver(){
+	// Teste Positivo para a ação subtração
+	@Test
+	public void testMinusSuccess() {
+
+		String n1, n2;
+		// Resupera valores
+		n1 = getDriver().findElement(By.id("number1")).getText();
+		n2 = getDriver().findElement(By.id("number2")).getText();
+
+		// Soma valores
+		getNewWebElement("result").sendKeys(
+				String.valueOf(Integer.valueOf(n1) - Integer.valueOf(n2)));
+		getDriver().findElement(By.id("sum")).click();
+
+		// Span de feedback do resultado
+		assertEquals("Resultado correto!",
+				getDriver().findElement(By.id("message")).getText());
+		waitTime(time);
+	}
+
+	// Teste Positivo para a ação multiplicação
+	@Test
+	public void testMultiplySuccess() {
+
+		String n1, n2;
+		// Resupera valores
+		n1 = getDriver().findElement(By.id("number1")).getText();
+		n2 = getDriver().findElement(By.id("number2")).getText();
+
+		// Soma valores
+		getNewWebElement("result").sendKeys(
+				String.valueOf(Integer.valueOf(n1) * Integer.valueOf(n2)));
+		getDriver().findElement(By.id("sum")).click();
+
+		// Span de feedback do resultado
+		assertEquals("Resultado correto!",
+				getDriver().findElement(By.id("message")).getText());
+		waitTime(time);
+	}
+
+	// Teste Positivo para a ação divide
+	@Test
+	public void testDivideSuccess() {
+
+		String n1, n2;
+		// Resupera valores
+		n1 = getDriver().findElement(By.id("number1")).getText();
+		n2 = getDriver().findElement(By.id("number2")).getText();
+
+		// Soma valores
+		getNewWebElement("result").sendKeys(
+				String.valueOf(Integer.valueOf(n1) / Integer.valueOf(n2)));
+		getDriver().findElement(By.id("sum")).click();
+
+		// Span de feedback do resultado
+		assertEquals("Resultado correto!",
+				getDriver().findElement(By.id("message")).getText());
+		waitTime(time);
+	}
+
+	private WebDriver getDriver() {
 		return this.driver;
 	}
-	
-	private void newInstanceWebDriver(RemoteWebDriver navigator, String url){
-		this.driver = navigator; 
+
+	private void newInstanceWebDriver(RemoteWebDriver navigator, String url) {
+		this.driver = navigator;
 		this.driver.get(url);
 	}
-	
-	private WebElement getNewWebElement(String name){
+
+	private WebElement getNewWebElement(String name) {
 		WebElement webElement = getDriver().findElement(By.id(name));
-	    return webElement;
+		return webElement;
 	}
-	
+
 	private void waitTime(long time) {
 		try {
 			Thread.sleep(time);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		};
+		}
+		;
 	}
-	
+
 	@After
-	public void closeDriver(){
+	public void closeDriver() {
 		getDriver().close();
 	}
 
